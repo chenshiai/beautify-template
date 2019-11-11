6f6f82df86d1be2d34a78f3ea371de1c90000552
陈帅 <chenshuai1@u51.com>
2019-11-11 20:26:26 +0800

    bugfix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,11 +19,11 @@
       <div class="battle-detail">
         <ul class="config">
           <li>
-            <img src="../assets/self.svg" @click="lookMyself">
+            <img src="../../public/img/self.svg" @click="lookMyself">
             <span class="config-detail">个人显示开关</span>
           </li>
           <li>
-            <img src="../assets/setting.svg" alt="">
+            <img src="../../public/img/setting.svg" alt="">
             <span class="config-detail">设置开发中</span>
           </li>
         </ul>
@@ -63,7 +63,7 @@
               {{ item.maxhit }}
               <span style="float: right;">
                 {{ item.deaths }}
-                <img src="../assets/kulou.svg" class="death-icon">
+                <img src="../../public/img/kulou.svg" class="death-icon">
               </span>
             </div>
           </div>
