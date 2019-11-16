df47c1344955dc813c0b51d4665c067d0767d750
ChenShiAi <1183942503@qq.com>
2019-11-16 13:13:57 +0800

    注释测试数据

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -164,7 +164,7 @@ export default class Home extends Vue {
     this.$router.push('config');
   }
   private mounted(): void {
-    this.data = mockdata; // 测试用数据
+    // this.data = mockdata; // 测试用数据
     document.addEventListener('onOverlayDataUpdate', (act) => {
       this.updateTemplate(act);
     });
