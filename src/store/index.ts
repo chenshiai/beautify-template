a1a44e90f521537af89565d9d96869acea1f4601
陈帅 <chenshuai1@u51.com>
2019-11-22 19:03:20 +0800

    vuex改成轻量的eventbus

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import showConfigs from './modules/showConfig';
-import geniusSister from './modules/geniusSister';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     showConfigs,
-    geniusSister,
   },
 });
